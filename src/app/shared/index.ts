2526355fb8b4f050b4a1d7c54732590d647a992d
jakubkot <jakubmikolaj.kot@gmail.com>
2021-12-07 19:34:21 +0100

    @pages

---
+++ src/app/shared/index.ts
@@ -0,0 +1,7 @@
+import { AppFilesUploadModule } from "./app-files-upload/app-files-upload.module";
+
+export const modules = [
+    AppFilesUploadModule,
+];
+
+export * from './app-files-upload/app-files-upload.module';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,19 @@
+import { modules as sharedModules } from "@shared/index";
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppFilesUploadModule } from './app-files-upload/app-files-upload.module';
-import { FilesUploadComponent } from './app-files-upload/components/files-upload/files-upload.component';
 
 @NgModule({
-  declarations: [
-  ],
   imports: [
-    AppFilesUploadModule,
+    ...sharedModules,
   ],
   exports: [
     CommonModule,
     HttpClientModule,
     BrowserAnimationsModule,
 
-    FilesUploadComponent,
+    ...sharedModules,
   ],
 })
 export class SharedModule { }
